08dafd7decea8ec89eabe94a60bd2a37266e7211
ParenkovLocal <parenkov.fedor@gmail.com>
2021-11-12 17:28:51 +0400

    AppConfig edit, added Configuration.baseUrl to DriverSettings

--- src/test/java/parenkov/tests/HeaderTests.java
+++ src/test/java/parenkov/tests/HeaderTests.java
@@ -16,7 +16,7 @@ import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Story("Header главной страницы")
+@Feature("Header главной страницы")
 public class HeaderTests extends TestBase {
 
     @Severity(SeverityLevel.MINOR)
