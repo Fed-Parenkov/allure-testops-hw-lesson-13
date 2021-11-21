36e904e27f2fb8af5a470929a4b9ec12006e34cd
Fedor Parenkov <85241814+Fed-Parenkov@users.noreply.github.com>
2021-11-21 11:43:43 +0400

    Update AllureAttachments.java

--- src/test/java/parenkov/helpers/AllureAttachments.java
+++ src/test/java/parenkov/helpers/AllureAttachments.java
@@ -38,7 +38,7 @@ public class AllureAttachments {
         URL videoUrl = DriverUtils.getVideoUrl(sessionId);
         if (videoUrl != null) {
             InputStream videoInputStream = null;
-            sleep(4000);
+            sleep(8000);
 
             for (int i = 0; i < 10; i++) {
                 try {
